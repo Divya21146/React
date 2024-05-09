1493c97be181cd594458a2bfb699438af8641750
Divi21146 <divi21146@gmail.com>
2024-05-09 12:17:17 +0530

    useState in button

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FavoriteColor from "./components/useState";
+
+function App() {
+  return (
+    <div>
+      <FavoriteColor/>
+    </div>
+  );
+}
+
+export default App;
