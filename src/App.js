1467c948f7e67c6d6d043b4a7d39e8d218769aab
Divi21146 <divi21146@gmail.com>
2024-05-09 15:28:22 +0530

    CRUD Operation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-import FavoriteColor from "./components/useState";
+import Home from "./components/home";
+import Add from './components/add';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import Edit from './components/edit';
 
 function App() {
   return (
-    <div>
-      <FavoriteColor/>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/create' element={<Add />} />
+          <Route path='/edit' element={<Edit />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/employees.js
@@ -0,0 +1,20 @@
+const Employees = [
+    {
+        id:"1",
+        Name:"Divya",
+        Age:"21"
+    },
+    {
+        id:"2",
+        Name:"Ashirvadh",
+        Age:"25"
+    },
+    {
+        id:"3",
+        Name:"Arokya",
+        Age:"18"
+    }
+]
+
+
+export default Employees
